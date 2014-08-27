52c446eef2d6263e74d4fdeec340d7da79881a6c
Piyal Basu <pbasu235@gmail.com>
2014-08-26 23:56:53 -0400

    Added devices and added post

--- js/main.js
+++ js/main.js
@@ -1,13 +1,46 @@
 var signOut = angular.module('signOut', []);
 
-signOut.factory('Roster', function($http){
-	var Roster = {};
-	$http.get('json/roster.json').success(function(data){
-		Roster.members = data;
+// signOut.factory('Roster', function($http){
+// 	var Roster = {};
+// 	$http.get('json/roster.json').success(function(data){
+// 		Roster.members = data;
+// 	});
+// 	return Roster;
+// });
+
+signOut.factory('Devices', function($http){
+	var Devices = {};
+	$http.get('json/devices.json').success(function(data){
+		Devices.list = data;
 	});
-	return Roster;
+	return Devices;
 });
 
-function RosterCtrl($scope, Roster) {
-	$scope.roster = Roster;
+
+signOut.controller('RosterCtrl', function($scope, $http) {
+	$http.get('json/roster.json').success(function(data){
+		$scope.roster = data;
+	});
+	
+
+	$scope.save = function() {
+    $http.post('json/roster.json', $scope.roster).then(function(data) {
+     $scope.msg = 'Data sent: '+ JSON.stringify($scope.roster[0].email);
+    });
+    //$scope.msg = 'Data sent: '+ JSON.stringify($scope.languages);
+  };
+});
+
+function DeviceCtrl($scope, Devices) {
+	$scope.devices = Devices;
 }
+
+signOut.directive("enter", function() {
+    return function(scope, element) {
+        element.bind("click", function() {
+            document.getElementById('devices').style.display = 'block';
+        })
+    }
+});
+
+
